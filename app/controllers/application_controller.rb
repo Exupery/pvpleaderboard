ae46f8a455c9f2f9ef8d08b93f607e7f6a85458f
Matthew Frost <mf@matthewfrost.com>
2015-02-21 12:22:14 -0500

    add title and description for index

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,8 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  def index
+  	@title = "WoW PvP Leaderboard"
+  	@description = "See which talents, glyphs, and stats top WoW PvPers are selecting"
+  end
 end
