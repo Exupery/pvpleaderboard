e41965790d84ebd6d4ccfa1a839f6f9fdb20ceed
Matthew Frost <mf@matthewfrost.com>
2015-02-22 18:20:07 -0500

    find player counts by faction

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -14,24 +14,39 @@ class OverviewController < ApplicationController
   	title_bracket = bracket.eql?("rbg") ? "RBG" : bracket
   	@title = "#{title_bracket || 'Leaderboard'} Overview"
   	@description = "WoW PvP leaderboard overview"
-  	
+
+  	@counts = Hash.new(0)
   	if bracket.nil?
   		@@BRACKETS.each do |b|
-  			faction_counts b
+  			fc = faction_counts b
+  			fc.each do |f, c|
+  				@counts[f] = @counts[f] + c
+  			end
   		end
   	else
-  		faction_counts bracket
+  		@counts = faction_counts bracket
   	end
-  	@bracket = bracket || "All"
+
+  	case bracket
+  	when nil
+  		@bracket = "All Leaderboards"
+  	when "rbg"
+  		@bracket = "Rated Battlegrounds"
+  	else
+  		@bracket = bracket
+  	end
   end
 
   private
 
   def faction_counts bracket
-  	# TODO
-  	rows = @@db.execute("SELECT factions.name AS faction, COUNT(*) FROM players JOIN factions ON players.faction_id=factions.id GROUP BY faction")
+  	h = Hash.new
+
+  	rows = @@db.execute("SELECT factions.name AS faction, COUNT(*) FROM bracket_#{bracket} JOIN players ON player_id=players.id JOIN factions ON players.faction_id=factions.id GROUP BY faction ORDER BY faction ASC")
   	rows.each do |row|
-  		puts row 	## TODO DELME
+  		h[row["faction"]] = row["count"].to_i
   	end
+
+  	return h
 	end
 end
