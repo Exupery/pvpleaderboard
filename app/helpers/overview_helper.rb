7692a26bceba571d8343a9333044972d950a169b
Matthew Frost <mf@matthewfrost.com>
2015-02-22 19:32:15 -0500

    display player counts by faction

--- app/helpers/overview_helper.rb
+++ app/helpers/overview_helper.rb
@@ -1,2 +1,13 @@
 module OverviewHelper
+	def faction_percent
+		h = Hash.new
+		total = 0
+		@counts.each do |f, c|
+			total += c
+		end
+		@counts.each do |f, c|
+			h[f] = (c.to_f / total * 100).round(1)
+		end
+		return h
+	end
 end
