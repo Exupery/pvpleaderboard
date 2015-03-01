a8e8ec8bfa72dd0165a98994df77a867fed3688c
Matthew Frost <mf@matthewfrost.com>
2015-03-01 16:49:45 -0500

    highlight current page in navbar

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+	def is_active path
+		return request.env["PATH_INFO"] == path ? "active" : "" if path == "/"
+		return request.env["PATH_INFO"].start_with?(path) ? "active" : ""
+	end
 end
