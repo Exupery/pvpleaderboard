99feeed9ac09d936a75051967adf3a2f7df22d3a
Matthew Frost <mf@matthewfrost.com>
2015-03-21 14:26:08 -0400

    select class and spec for glyphs

--- app/controllers/glyphs_controller.rb
+++ app/controllers/glyphs_controller.rb
@@ -5,5 +5,36 @@ class GlyphsController < ApplicationController
 	def glyphs_by_class
 		@title = "Glyphs"
 		@description = "WoW PvP leaderboard glyph choices"
+		@heading = "Select a Class"
+
+		classes = Classes.list
+		@class_slug = slugify params[:class]
+		if @class_slug
+			if !classes.key?(@class_slug)
+				redirect_to "/glyphs"
+				return nil
+			end
+			clazz = classes[@class_slug]["name"]
+			@title = "#{@clazz} Glyphs"
+			@description = @title
+			@heading = "Select a Specialization"
+		end
+
+		@spec_slug = slugify params[:spec]
+		if @spec_slug
+			spec_slugs = Specs.slugs
+			full_slug = "#{@class_slug}_#{@spec_slug}"
+			if !spec_slugs.key?(full_slug)
+				redirect_to "/glyphs/#{@class_slug}"
+				return nil
+			end
+			@title = "#{spec_slugs[full_slug]["name"]} #{clazz} Glyphs"
+			@description = @title
+			@heading = @title
+
+			class_id = classes[@class_slug]["id"]
+			spec_id = spec_slugs[full_slug]["id"]
+		end
 	end
+	
 end
