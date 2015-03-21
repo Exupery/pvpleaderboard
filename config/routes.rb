2add62f85f12fe544d540256e84796628d76158f
Matthew Frost <mf@matthewfrost.com>
2015-03-21 11:22:26 -0400

    added glyph routing

--- config/routes.rb
+++ config/routes.rb
@@ -3,56 +3,12 @@ Rails.application.routes.draw do
 
   get 'overview' => 'overview#stats'
   get 'overview/:bracket' => 'overview#stats'
+
   get 'talents' => 'talents#talents_by_class'
   get 'talents/:class' => 'talents#talents_by_class'
   get 'talents/:class/:spec' => 'talents#talents_by_class'
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
-
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+  get 'glyphs' => 'glyphs#glyphs_by_class'
+  get 'glyphs/:class' => 'glyphs#glyphs_by_class'
+  get 'glyphs/:class/:spec' => 'glyphs#glyphs_by_class'
 end
