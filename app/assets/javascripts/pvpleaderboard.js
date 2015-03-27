d418b1f3893e964c2c8e314a4ecb8b9846b072c8
Matthew Frost <mf@matthewfrost.com>
2015-03-27 17:22:36 -0400

    filter spec selector

--- app/assets/javascripts/pvpleaderboard.js
+++ app/assets/javascripts/pvpleaderboard.js
@@ -1,6 +1,31 @@
 var ready = function() {
   $(".table-sorted").tablesorter({sortInitialOrder: "desc"});
+
+  $(".class-selector").mouseenter(function() {
+    var classSlug = cssify($(this).data("class-slug"));
+    $(".spec-group").hide();
+    $("#" + classSlug + "-specs").show();
+  });
+
+  $(".class-selector").click(function() {
+    $(".spec-selector").removeClass("active");
+  });
+
+  $(".class-selector-group").mouseleave(function() {
+    if ($(".class-selector").hasClass("active")) {
+      var classSlug = cssify($(".class-selector.active").first().data("class-slug"));
+      $(".spec-group").hide();
+      $("#" + classSlug + "-specs").show();
+    } else {
+      $(".spec-group").hide();
+      $(".placeholder-spec-group").show();
+    }
+  });
 };
 /* Needed so jQuery's ready plays well with Rails turbolinks */
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+function cssify(str) {
+  return str.toLowerCase().replace(/[\s_]/g, "-");
+}
