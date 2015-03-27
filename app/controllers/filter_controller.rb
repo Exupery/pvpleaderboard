c38476a756f1dc7e8fe2c83d70a612179aded342
Matthew Frost <mf@matthewfrost.com>
2015-03-26 21:30:13 -0400

    class and leaderboard selector for filter

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -2,6 +2,8 @@ class FilterController < ApplicationController
   protect_from_forgery with: :exception
 
   def filter
+    @title = "Filter"
+    @description = "WoW PvP leaderboard Talents, Glyphs, and Stats"
     puts params ## TODO DELME
   end
 end
