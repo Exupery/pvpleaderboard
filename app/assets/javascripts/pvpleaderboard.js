5d762ac04e993910ec0cc6c7697c1a0e84e4a287
Matthew Frost <mf@matthewfrost.com>
2015-03-28 18:12:58 -0400

    advanced filtering toggling

--- app/assets/javascripts/pvpleaderboard.js
+++ app/assets/javascripts/pvpleaderboard.js
@@ -25,10 +25,36 @@ var ready = function() {
   $(".unactivater").click(function() {
     $("." + $(this).data("unactivate")).removeClass("active");
   });
+
+  $(".collapse").on("shown.bs.collapse", function() {
+    $(".collapse .btn-group").each(function() {
+      var first = $(this).children().first();
+      var last = $(this).children().last();
+      if (first.offset()["top"] != last.offset()["top"]) {
+        first.css({"border-bottom-left-radius": 0, "margin-left": "-1px"});
+        last.css("border-top-right-radius", 0);
+      }
+    });
+  });
+
+  $(".collapse").on("show.bs.collapse", function() {
+    toggleCollapser($(this).data("toggler"), false);
+  });
+
+  $(".collapse").on("hide.bs.collapse", function() {
+    toggleCollapser($(this).data("toggler"), true);
+  });
 };
 /* Needed so jQuery's ready plays well with Rails turbolinks */
 $(document).ready(ready);
 $(document).on('page:load', ready);
+
+function toggleCollapser(id, collapsed) {
+  var text = (collapsed) ? "Show" : "Hide";
+  var toggler = $("#" + id);
+  toggler.width(toggler.width());
+  toggler.text(text + " " + $(toggler).data("text"));
+}
 
 function cssify(str) {
   return str.toLowerCase().replace(/[\s_]/g, "-");
