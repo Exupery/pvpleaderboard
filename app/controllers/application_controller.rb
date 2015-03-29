f3c35d036bae73445d5bb9de9be7158d976f6718
Matthew Frost <mf@matthewfrost.com>
2015-03-29 19:52:32 -0400

    append site name to title

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,8 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  
+
   def index
-  	@title = "WoW PvP Leaderboard"
+  	@title = "Home"
   	@description = "See which talents, glyphs, and stats top WoW PvPers are selecting"
   end
 end
