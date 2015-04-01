b769bb17ddab2899ea6cff57191e633170d7c83c
Matthew Frost <mf@matthewfrost.com>
2015-03-31 23:09:10 -0400

    return early if no players found

--- app/controllers/filter_controller.rb
+++ app/controllers/filter_controller.rb
@@ -9,6 +9,23 @@ class FilterController < ApplicationController
   def results
     @title = "Filter Results"
     @description = "WoW PvP leaderboard Talents, Glyphs, and Stats Filter Results"
+
+    player_ids = find_player_ids
+    return nil if player_ids.empty?
+    puts player_ids.length  ## TODO DELME
+  end
+
+  private
+
+  def find_player_ids
+    ids = Array.new
     puts params ## TODO DELME
+
+    # rows = ActiveRecord::Base.connection.execute("SELECT id FROM players LIMIT 5")
+    # rows.each do |row|
+    #   ids.push(row["id"])
+    # end
+
+    return ids
   end
 end
