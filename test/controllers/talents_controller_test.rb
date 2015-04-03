fa2f7e8097d9946aa12ef75f7eca62cb8162297f
Matthew Frost <mf@matthewfrost.com>
2015-04-02 20:26:32 -0400

    talents by class test

--- test/controllers/talents_controller_test.rb
+++ test/controllers/talents_controller_test.rb
@@ -7,4 +7,20 @@ class TalentsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:title)
     assert_not_nil assigns(:description)
   end
+
+  test "should show talents by class" do
+    get(:talents_by_class, {"class" => "druid", "spec" => "spec10"})
+    assert_response :success
+
+    assert_not_nil assigns(:class_slug)
+    assert_not_nil assigns(:spec_slug)
+    assert_not_nil assigns(:counts)
+    assert_not_nil assigns(:total)
+    assert_not_nil assigns(:class_talents)
+
+    assert_not_empty :counts
+    assert_not_empty :class_talents
+
+    assert_not_equal(0, :total)
+  end
 end
