9ee067dbabc69f0d7c73f23bb822c4a1fcfb3ef4
Matthew Frost <mf@matthewfrost.com>
2015-04-02 20:16:10 -0400

    glyphs by class test

--- test/controllers/glyphs_controller_test.rb
+++ test/controllers/glyphs_controller_test.rb
@@ -7,4 +7,20 @@ class GlyphsControllerTest < ActionController::TestCase
     assert_not_nil assigns(:title)
     assert_not_nil assigns(:description)
   end
+
+  test "should show glyphs by class" do
+    get(:glyphs_by_class, {"class" => "warlock", "spec" => "spec8"})
+    assert_response :success
+
+    assert_not_nil assigns(:class_slug)
+    assert_not_nil assigns(:spec_slug)
+    assert_not_nil assigns(:major_counts)
+    assert_not_nil assigns(:total)
+    assert_not_nil assigns(:class_glyphs)
+
+    assert_not_empty :major_counts
+    assert_not_empty :class_glyphs
+
+    assert_not_equal(0, :total)
+  end
 end
