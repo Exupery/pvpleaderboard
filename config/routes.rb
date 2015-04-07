a6260be1da9d6fc7abbe2aab589ff7634b4a4910
Matthew Frost <mf@matthewfrost.com>
2015-04-07 19:41:27 -0400

    use double quotes in routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,21 @@
 Rails.application.routes.draw do
-  root 'application#index'
+  root "application#index"
 
-  get 'overview' => 'overview#stats'
-  get 'overview/:bracket' => 'overview#stats'
+  get "overview" => "overview#stats"
+  get "overview/:bracket" => "overview#stats"
 
-  get 'talents' => 'talents#talents_by_class'
-  get 'talents/:class' => 'talents#talents_by_class'
-  get 'talents/:class/:spec' => 'talents#talents_by_class'
+  get "talents" => "talents#talents_by_class"
+  get "talents/:class" => "talents#talents_by_class"
+  get "talents/:class/:spec" => "talents#talents_by_class"
 
-  get 'glyphs' => 'glyphs#glyphs_by_class'
-  get 'glyphs/:class' => 'glyphs#glyphs_by_class'
-  get 'glyphs/:class/:spec' => 'glyphs#glyphs_by_class'
+  get "glyphs" => "glyphs#glyphs_by_class"
+  get "glyphs/:class" => "glyphs#glyphs_by_class"
+  get "glyphs/:class/:spec" => "glyphs#glyphs_by_class"
 
-  get 'stats' => 'stats#stats_by_class'
-  get 'stats/:class' => 'stats#stats_by_class'
-  get 'stats/:class/:spec' => 'stats#stats_by_class'
+  get "stats" => "stats#stats_by_class"
+  get "stats/:class" => "stats#stats_by_class"
+  get "stats/:class/:spec" => "stats#stats_by_class"
 
-  get 'filter' => 'filter#filter'
-  get 'filter/results' => 'filter#results'
+  get "filter" => "filter#filter"
+  get "filter/results" => "filter#results"
 end
