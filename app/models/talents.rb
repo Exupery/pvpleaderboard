bdf585547dba89806b855a8b61b043171f07e863
Matthew Frost <mf@matthewfrost.com>
2015-04-14 20:19:13 -0400

    added wowhead tooltips for talents

--- app/models/talents.rb
+++ app/models/talents.rb
@@ -4,7 +4,7 @@ class Talents
 
 		rows = ActiveRecord::Base.connection.execute("SELECT id, name, description, icon, tier, col FROM talents WHERE class_id=#{class_id}")
     rows.each do |row|
-      h[row["id"]] = {:name => row["name"], :description => row["description"], :icon => row["icon"], :tier => row["tier"], :col => row["col"]}
+      h[row["id"]] = {:id => row["id"], :name => row["name"], :description => row["description"], :icon => row["icon"], :tier => row["tier"], :col => row["col"]}
     end
 
     return h
