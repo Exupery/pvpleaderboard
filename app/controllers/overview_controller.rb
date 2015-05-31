18afec474affdf88997e37166878a105bd35c7bf
Matthew Frost <mf@matthewfrost.com>
2015-05-31 10:14:25 -0400

    find realm counts

--- app/controllers/overview_controller.rb
+++ app/controllers/overview_controller.rb
@@ -18,6 +18,7 @@ class OverviewController < ApplicationController
     @races = Hash.new(0)
     @classes = Hash.new(0)
     @specs = Hash.new(nil)
+    @realms = Hash.new(0)
 
     find_counts bracket
 
@@ -36,28 +37,28 @@ class OverviewController < ApplicationController
   def find_counts bracket
     if bracket.nil?
       @@BRACKETS.each do |b|
-        fc = faction_counts b
-        fc.each do |f, c|
+        (faction_counts b).each do |f, c|
           @factions[f] += c
         end
 
-        rc = race_counts b
-        rc.each do |r, c|
+        (race_counts b).each do |r, c|
           @races[r] += c
         end
 
-        cc = class_counts b
-        cc.each do |cl, c|
+        (class_counts b).each do |cl, c|
           @classes[cl] += c
         end
 
-        sc = spec_counts b
-        sc.each do |s, info|
+        (spec_counts b).each do |s, info|
           if @specs[s].nil?
             @specs[s] = info
           else
             @specs[s].count += info.count
           end
+        end
+
+        (realm_counts b).each do |r, c|
+          @realms[r] += c
         end
       end
     else
@@ -65,6 +66,7 @@ class OverviewController < ApplicationController
       @races = race_counts bracket
       @classes = class_counts bracket
       @specs = spec_counts bracket
+      @realms = realm_counts bracket
     end
   end
 
@@ -107,6 +109,17 @@ class OverviewController < ApplicationController
     rows = ActiveRecord::Base.connection.execute("SELECT specs.name AS spec, specs.icon, classes.name AS class, COUNT(*) FROM bracket_#{bracket} JOIN players ON player_id=players.id JOIN specs ON players.spec_id=specs.id JOIN classes ON specs.class_id=classes.id GROUP BY spec, specs.icon, class ORDER BY spec ASC")
     rows.each do |row|
       h[row["class"] + row["spec"]] = SpecInfo.new(row["spec"], row["count"].to_i, row["icon"], row["class"])
+    end
+
+    return h
+  end
+
+  def realm_counts bracket
+    h = Hash.new
+
+    rows = ActiveRecord::Base.connection.execute("SELECT realms.name AS realm, COUNT(*) FROM bracket_#{bracket} JOIN players ON player_id=players.id JOIN realms ON players.realm_slug=realms.slug GROUP BY realm ORDER BY COUNT(*) DESC LIMIT 25")
+    rows.each do |row|
+      h[row["realm"]] = row["count"].to_i
     end
 
     return h
