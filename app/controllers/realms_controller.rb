9aaa78c0c08756d703369b40fe22d0f47b2de485
Matthew Frost <mf@matthewfrost.com>
2016-01-10 18:45:04 -0500

    get all realm counts for each/all brackets

--- app/controllers/realms_controller.rb
+++ app/controllers/realms_controller.rb
@@ -1,7 +1,24 @@
-class RealmsController < ApplicationController
+class RealmsController < OverviewController
 
   def show
-    puts "TODO"
+    bracket = get_bracket
+    title_bracket = bracket.eql?("rbg") ? "RBG" : bracket
+    @title = "All Realms #{title_bracket || 'All Brackets'} Overview"
+    @description = "World of Warcraft PvP leaderboard realms"
+
+    @realms = Hash.new(0)
+
+    if bracket.nil?
+      @@BRACKETS.each do |b|
+        (realm_counts(b, "ALL")).each do |r, c|
+          @realms[r] += c
+        end
+      end
+    else
+      @realms = realm_counts(bracket, "ALL")
+    end
+
+    set_bracket bracket
   end
 
 end
