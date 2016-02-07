8423c7ab21b7f31f12928c7d1709f9c57e1ababb
Matthew Frost <mf@matthewfrost.com>
2016-02-07 15:45:42 -0500

    move filter query to FilterUtils

--- app/controllers/concerns/filter_utils.rb
+++ app/controllers/concerns/filter_utils.rb
@@ -1,4 +1,16 @@
 module FilterUtils extend ActiveSupport::Concern
+  def filter_leaderboard(bracket, where)
+    players = Array.new
+
+    rows = ActiveRecord::Base.connection.execute("SELECT players.id AS id, ranking, rating, season_wins AS wins, season_losses AS losses, players.name AS name, factions.name AS faction, races.name AS race, players.gender AS gender, classes.name AS class, specs.name AS spec, specs.icon AS spec_icon, realms.slug AS realm_slug, realms.name AS realm, players.guild FROM bracket_#{bracket} LEFT JOIN players ON bracket_#{bracket}.player_id=players.id LEFT JOIN factions ON players.faction_id=factions.id LEFT JOIN races ON players.race_id=races.id LEFT JOIN classes on players.class_id=classes.id LEFT JOIN specs ON players.spec_id=specs.id LEFT JOIN realms ON players.realm_slug=realms.slug #{where} ORDER BY ranking ASC")
+
+    rows.each do |row|
+      players << Player.new(row)
+    end
+
+    return players
+  end
+
   def create_where_clause
     where = ""
 
