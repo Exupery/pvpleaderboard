7e28e8bc75195b547f0d99237a5c868e193f1816
Matthew Frost <mf@matthewfrost.com>
2016-08-28 11:49:12 -0400

    use talent spell ID when linking to wowhead

--- app/models/talents.rb
+++ app/models/talents.rb
@@ -2,9 +2,9 @@ class Talents
 	def self.get_talents class_id
 		h = Hash.new()
 
-		rows = ActiveRecord::Base.connection.execute("SELECT id, name, description, icon, tier, col FROM talents WHERE class_id=#{class_id}")
+		rows = ActiveRecord::Base.connection.execute("SELECT id, spell_id, spec_id, name, description, icon, tier, col FROM talents WHERE class_id=#{class_id}")
     rows.each do |row|
-      h[row["id"]] = {:id => row["id"], :name => row["name"], :description => row["description"], :icon => row["icon"], :tier => row["tier"], :col => row["col"]}
+      h[row["id"]] = {:id => row["id"], :spell_id => row["spell_id"], :spec_id => row["spec_id"], :name => row["name"], :description => row["description"], :icon => row["icon"], :tier => row["tier"], :col => row["col"]}
     end
 
     return h
