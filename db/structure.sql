e26a8988eb415ba57e489eaa6911f9a27df4b211
Matthew Frost <mf@matthewfrost.com>
2017-05-07 14:47:57 -0400

    removed superfluous alter owner statements

--- db/structure.sql
+++ db/structure.sql
@@ -31,10 +31,6 @@ SET default_tablespace = '';
 
 SET default_with_oids = false;
 
---
--- Name: achievements; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE achievements (
     id integer NOT NULL,
     name character varying(128),
@@ -43,36 +39,15 @@ CREATE TABLE achievements (
     points smallint
 );
 
-
-ALTER TABLE achievements OWNER TO frost;
-
---
--- Name: classes; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE classes (
     id integer NOT NULL,
     name character varying(32) NOT NULL
 );
 
-
-ALTER TABLE classes OWNER TO frost;
-
---
--- Name: factions; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE factions (
     id integer NOT NULL,
     name character varying(32) NOT NULL
 );
-
-
-ALTER TABLE factions OWNER TO frost;
-
---
--- Name: leaderboards; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE leaderboards (
     bracket character(3) NOT NULL,
@@ -85,25 +60,11 @@ CREATE TABLE leaderboards (
     last_update timestamp without time zone DEFAULT now()
 );
 
-
-ALTER TABLE leaderboards OWNER TO frost;
-
---
--- Name: metadata; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE metadata (
     key character varying(32) NOT NULL,
     value character varying(512) DEFAULT ''::character varying NOT NULL,
     last_update timestamp without time zone DEFAULT now()
 );
-
-
-ALTER TABLE metadata OWNER TO frost;
-
---
--- Name: players; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE players (
     id integer NOT NULL,
@@ -121,25 +82,11 @@ CREATE TABLE players (
     last_update timestamp without time zone DEFAULT now() NOT NULL
 );
 
-
-ALTER TABLE players OWNER TO frost;
-
---
--- Name: players_achievements; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE players_achievements (
     player_id integer NOT NULL,
     achievement_id integer NOT NULL,
     achieved_at timestamp without time zone
 );
-
-
-ALTER TABLE players_achievements OWNER TO frost;
-
---
--- Name: players_id_seq; Type: SEQUENCE; Schema: public; Owner: frost
---
 
 CREATE SEQUENCE players_id_seq
     START WITH 1
@@ -148,44 +95,18 @@ CREATE SEQUENCE players_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE players_id_seq OWNER TO frost;
-
---
--- Name: players_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: frost
---
-
 ALTER SEQUENCE players_id_seq OWNED BY players.id;
-
-
---
--- Name: players_talents; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE players_talents (
     player_id integer NOT NULL,
     talent_id integer NOT NULL
 );
 
-
-ALTER TABLE players_talents OWNER TO frost;
-
---
--- Name: races; Type: TABLE; Schema: public; Owner: frost
---
-
 CREATE TABLE races (
     id integer NOT NULL,
     name character varying(32) NOT NULL,
     side character varying(32) NOT NULL
 );
-
-
-ALTER TABLE races OWNER TO frost;
-
---
--- Name: realms; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE realms (
     id integer NOT NULL,
@@ -197,13 +118,6 @@ CREATE TABLE realms (
     type character varying(16)
 );
 
-
-ALTER TABLE realms OWNER TO frost;
-
---
--- Name: realms_id_seq; Type: SEQUENCE; Schema: public; Owner: frost
---
-
 CREATE SEQUENCE realms_id_seq
     START WITH 1
     INCREMENT BY 1
@@ -211,19 +125,7 @@ CREATE SEQUENCE realms_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE realms_id_seq OWNER TO frost;
-
---
--- Name: realms_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: frost
---
-
 ALTER SEQUENCE realms_id_seq OWNED BY realms.id;
-
-
---
--- Name: specs; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE specs (
     id integer NOT NULL,
@@ -234,13 +136,6 @@ CREATE TABLE specs (
     background_image character varying(128),
     icon character varying(128)
 );
-
-
-ALTER TABLE specs OWNER TO frost;
-
---
--- Name: talents; Type: TABLE; Schema: public; Owner: frost
---
 
 CREATE TABLE talents (
     id integer NOT NULL,
@@ -254,13 +149,6 @@ CREATE TABLE talents (
     col smallint
 );
 
-
-ALTER TABLE talents OWNER TO frost;
-
---
--- Name: talents_id_seq; Type: SEQUENCE; Schema: public; Owner: frost
---
-
 CREATE SEQUENCE talents_id_seq
     START WITH 1
     INCREMENT BY 1
@@ -268,358 +156,130 @@ CREATE SEQUENCE talents_id_seq
     NO MAXVALUE
     CACHE 1;
 
-
-ALTER TABLE talents_id_seq OWNER TO frost;
-
---
--- Name: talents_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: frost
---
-
 ALTER SEQUENCE talents_id_seq OWNED BY talents.id;
-
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players ALTER COLUMN id SET DEFAULT nextval('players_id_seq'::regclass);
 
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY realms ALTER COLUMN id SET DEFAULT nextval('realms_id_seq'::regclass);
 
-
---
--- Name: id; Type: DEFAULT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY talents ALTER COLUMN id SET DEFAULT nextval('talents_id_seq'::regclass);
-
-
---
--- Name: achievements_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY achievements
     ADD CONSTRAINT achievements_pkey PRIMARY KEY (id);
 
-
---
--- Name: classes_name_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY classes
     ADD CONSTRAINT classes_name_key UNIQUE (name);
-
-
---
--- Name: classes_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY classes
     ADD CONSTRAINT classes_pkey PRIMARY KEY (id);
 
-
---
--- Name: factions_name_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY factions
     ADD CONSTRAINT factions_name_key UNIQUE (name);
-
-
---
--- Name: factions_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY factions
     ADD CONSTRAINT factions_pkey PRIMARY KEY (id);
 
-
---
--- Name: leaderboards_bracket_region_player_id_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY leaderboards
     ADD CONSTRAINT leaderboards_bracket_region_player_id_key UNIQUE (bracket, region, player_id);
-
-
---
--- Name: leaderboards_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY leaderboards
     ADD CONSTRAINT leaderboards_pkey PRIMARY KEY (bracket, region, ranking);
 
-
---
--- Name: metadata_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY metadata
     ADD CONSTRAINT metadata_pkey PRIMARY KEY (key);
-
-
---
--- Name: players_achievements_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players_achievements
     ADD CONSTRAINT players_achievements_pkey PRIMARY KEY (player_id, achievement_id);
 
-
---
--- Name: players_name_realm_id_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_name_realm_id_key UNIQUE (name, realm_id);
-
-
---
--- Name: players_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_pkey PRIMARY KEY (id);
 
-
---
--- Name: players_talents_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players_talents
     ADD CONSTRAINT players_talents_pkey PRIMARY KEY (player_id, talent_id);
-
-
---
--- Name: races_name_side_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY races
     ADD CONSTRAINT races_name_side_key UNIQUE (name, side);
 
-
---
--- Name: races_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY races
     ADD CONSTRAINT races_pkey PRIMARY KEY (id);
-
-
---
--- Name: realms_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY realms
     ADD CONSTRAINT realms_pkey PRIMARY KEY (id);
 
-
---
--- Name: realms_slug_region_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY realms
     ADD CONSTRAINT realms_slug_region_key UNIQUE (slug, region);
-
-
---
--- Name: specs_class_id_name_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY specs
     ADD CONSTRAINT specs_class_id_name_key UNIQUE (class_id, name);
 
-
---
--- Name: specs_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY specs
     ADD CONSTRAINT specs_pkey PRIMARY KEY (id);
-
-
---
--- Name: talents_id_key; Type: CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY talents
     ADD CONSTRAINT talents_id_key UNIQUE (id);
 
-
---
--- Name: talents_pkey; Type: CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY talents
     ADD CONSTRAINT talents_pkey PRIMARY KEY (spell_id, spec_id);
 
-
---
--- Name: achievements_name_idx; Type: INDEX; Schema: public; Owner: frost
---
-
 CREATE INDEX achievements_name_idx ON achievements USING btree (name);
-
-
---
--- Name: leaderboards_rating_idx; Type: INDEX; Schema: public; Owner: frost
---
 
 CREATE INDEX leaderboards_rating_idx ON leaderboards USING btree (rating);
 
-
---
--- Name: players_class_id_spec_id_idx; Type: INDEX; Schema: public; Owner: frost
---
-
 CREATE INDEX players_class_id_spec_id_idx ON players USING btree (class_id, spec_id);
-
-
---
--- Name: players_faction_id_race_id_idx; Type: INDEX; Schema: public; Owner: frost
---
 
 CREATE INDEX players_faction_id_race_id_idx ON players USING btree (faction_id, race_id);
 
-
---
--- Name: players_guild_idx; Type: INDEX; Schema: public; Owner: frost
---
-
 CREATE INDEX players_guild_idx ON players USING btree (guild);
-
-
---
--- Name: players_last_update_idx; Type: INDEX; Schema: public; Owner: frost
---
 
 CREATE INDEX players_last_update_idx ON players USING btree (last_update DESC);
 
-
---
--- Name: talents_class_id_spec_id_idx; Type: INDEX; Schema: public; Owner: frost
---
-
 CREATE INDEX talents_class_id_spec_id_idx ON talents USING btree (class_id, spec_id);
 
-
---
--- Name: talents_tier_col_idx; Type: INDEX; Schema: public; Owner: frost
---
-
 CREATE INDEX talents_tier_col_idx ON talents USING btree (tier, col);
-
-
---
--- Name: leaderboards_player_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY leaderboards
     ADD CONSTRAINT leaderboards_player_id_fkey FOREIGN KEY (player_id) REFERENCES players(id);
 
-
---
--- Name: players_achievements_achievement_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players_achievements
     ADD CONSTRAINT players_achievements_achievement_id_fkey FOREIGN KEY (achievement_id) REFERENCES achievements(id);
-
-
---
--- Name: players_achievements_player_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players_achievements
     ADD CONSTRAINT players_achievements_player_id_fkey FOREIGN KEY (player_id) REFERENCES players(id);
 
-
---
--- Name: players_class_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_class_id_fkey FOREIGN KEY (class_id) REFERENCES classes(id);
-
-
---
--- Name: players_faction_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_faction_id_fkey FOREIGN KEY (faction_id) REFERENCES factions(id);
 
-
---
--- Name: players_race_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_race_id_fkey FOREIGN KEY (race_id) REFERENCES races(id);
-
-
---
--- Name: players_realm_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_realm_id_fkey FOREIGN KEY (realm_id) REFERENCES realms(id);
 
-
---
--- Name: players_spec_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players
     ADD CONSTRAINT players_spec_id_fkey FOREIGN KEY (spec_id) REFERENCES specs(id);
-
-
---
--- Name: players_talents_player_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY players_talents
     ADD CONSTRAINT players_talents_player_id_fkey FOREIGN KEY (player_id) REFERENCES players(id);
 
-
---
--- Name: players_talents_talent_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY players_talents
     ADD CONSTRAINT players_talents_talent_id_fkey FOREIGN KEY (talent_id) REFERENCES talents(id);
-
-
---
--- Name: specs_class_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
 
 ALTER TABLE ONLY specs
     ADD CONSTRAINT specs_class_id_fkey FOREIGN KEY (class_id) REFERENCES classes(id);
 
-
---
--- Name: talents_class_id_fkey; Type: FK CONSTRAINT; Schema: public; Owner: frost
---
-
 ALTER TABLE ONLY talents
     ADD CONSTRAINT talents_class_id_fkey FOREIGN KEY (class_id) REFERENCES classes(id);
-
-
---
--- Name: public; Type: ACL; Schema: -; Owner: postgres
---
 
 REVOKE ALL ON SCHEMA public FROM PUBLIC;
 REVOKE ALL ON SCHEMA public FROM postgres;
 GRANT ALL ON SCHEMA public TO postgres;
 GRANT ALL ON SCHEMA public TO PUBLIC;
-
 
 --
 -- PostgreSQL database dump complete
