2e27a1d267d8d3b06e722b7312fc471b1d1bae40
Matthew Frost <Exupery@users.noreply.github.com>
2018-03-04 10:12:59 -0500

    check class icons in icon check

--- bin/icon_check.rb
+++ bin/icon_check.rb
@@ -2,24 +2,44 @@
 
 require "pg"
 
-BASE_PATH = "public/images/icons/"
+BASE_PATH = "public/images/"
+ICONS = "icons/"
+CLASSES = "classes/"
 TABLES = [ "specs", "talents" ]
 
 db = PG.connect(ENV["POSTGRESQL_DEV_URL"])
-has_missing = false
+$has_missing = false
+
+## Check if `icon_file` exists
+def check(icon_name, icon_file)
+  if !(File.exist?(icon_file))
+    puts "MISSING:" if !$has_missing
+    $has_missing = true
+    puts icon_name
+  end
+end
+
+## Check tables with icon field
 TABLES.each do |table|
   db.exec("SELECT DISTINCT(icon) FROM #{table}") do |result|
     result.each do |row|
       icon_name = row["icon"]
-      icon_file = "#{BASE_PATH + icon_name}.png"
+      icon_file = "#{BASE_PATH + ICONS + icon_name}.png"
 
-      if !(File.exist?(icon_file))
-        puts "MISSING:" if !has_missing
-        has_missing = true
-        puts icon_name
-      end
+      check(icon_name, icon_file)
     end
   end
 end
+
+## Check tables without icon fields
+db.exec("SELECT name FROM classes") do |result|
+  result.each do |row|
+    icon_name = row["name"].downcase.gsub(/\s/, "_")
+    icon_file = "#{BASE_PATH + CLASSES + icon_name}.png"
+
+    check(icon_name, icon_file)
+  end
+end
+
 db.close
-puts "All required icons present in #{BASE_PATH}" unless has_missing
+puts "All required icons present in #{BASE_PATH}" unless $has_missing
