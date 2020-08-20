d6af7d22a07aa608f66a9386e248b342f015f6be
Matthew Frost <Exupery@users.noreply.github.com>
2020-08-20 09:53:37 -0400

    set format to HTML for controllers

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,13 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :set_format
 
   @@BRACKETS = Brackets.list
   @@REGIONS = Regions.list
+
+  def set_format
+    request.format = "html"
+  end
 
   def index
     expires_in 1.week, public: true
