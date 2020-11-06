433bc6152f0534309ae085a769e1aabed86b23a6
Matthew Frost <Exupery@users.noreply.github.com>
2020-11-06 14:07:36 -0500

    use mem_cache_store instead of dalli_store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -56,7 +56,7 @@ Rails.application.configure do
   config.log_tags = [ :request_id ]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","), {
+  config.cache_store = :mem_cache_store, (ENV["MEMCACHIER_SERVERS"] || "").split(","), {
     :username => ENV["MEMCACHIER_USERNAME"],
     :password => ENV["MEMCACHIER_PASSWORD"],
     :expires_in => 1.hour,
